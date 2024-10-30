1f7e011f584c881e56fc6b3070fe820eada2bab9
Nickolas499 <ezequielalmonte499@gmail.com>
2024-10-30 16:02:51 -0400

    10/30/2024

--- client2/src/pages/dashboard/assiments/Assignmets.jsx
+++ client2/src/pages/dashboard/assiments/Assignmets.jsx
@@ -1,35 +1,73 @@
 import style from './assignment.module.css'
 import { useAuth } from '../../../context/AuthContext';
 
+
+const Assignmetsdata = [
+	{
+		user: "67212f2070f6c98bd78de32f",
+		Assignmets: [{ IBO: 5, }]
+	},
+	{
+		user: "67212f7270f6c98bd78de343",
+		Assignmets: [{ IBO: 5, DIGI_ABUT: 28, }]
+	},
+	{
+		user: "67212fc270f6c98bd78de348",
+		Assignmets: [{ PHIS_ABUT: 5, FULL_ARCH: 5 }]
+	},
+]
+
+
+
+
+
+
 const Assignmets = () => {
-  const { usuarios, GetUsers } = useAuth();
+	const { usuarios, } = useAuth();
 
 
 
-  return (
-    <div className={style.container}>
-      <h1>ASSIGMENTS</h1>
+	return (
+		<div className={style.container}>
+			<h1>ASSIGMENTS</h1>
 
-      <div className={style.userscards}>
-        {usuarios.map((user) => (
-          <div className={style.UserCard} key={user._id} style={{ backgroundColor: user.color + "66" }}>
-            <div className={style.CardTitle}>
-              <span className={style.initials} style={{ backgroundColor: user.color }}>{`${user.fname[0]}${user.lname[0]}`}</span>
-              <span className={style.UserName}>{user.fname}  {user.lname}</span>
-            </div>
+			<div className={style.userscards}>
+				{usuarios.map((user) => (
+					<div className={style.UserCard} key={user._id} style={{ borderColor: user.color + "90" }}>
+						<div className={style.CardTitle}>
+							<span className={style.initials} style={{ backgroundColor: user.color }}>{`${user.fname[0]}${user.lname[0]}`}</span>
+							<span className={style.UserName}>{user.fname}  {user.lname}</span>
+						</div>
+						<div className={style.Assignmetsdata}>
+							{Assignmetsdata.map((assign) => (
+								assign.user === user._id ? (
+									<div key={assign.user}>
+										{assign.Assignmets.map((item, index) => (
+											<div key={index} className={style.assign_container}>
+												{Object.entries(item).map(([key, value]) => (
+													<div key={key} className={style.assign_data}>
+														<span className={style.key}>{key}</span>
+														<span className={style.value}>{value}</span>
+													</div>
+												))}
+											</div>
+										))}
+									</div>
+								) : null // Retorna null si no se cumple la condición
+							))}
+						</div>
+						
 
-            <span>{user.username}</span>
-            <span>{user._id}</span>
-          </div>
 
 
+					</div>
+				))}
+			</div>
 
-        ))}
-      </div>
+		</div>
 
-    </div>
-
-  )
+	)
 }
 
 export default Assignmets
+
